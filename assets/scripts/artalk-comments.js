c4c5652e2295beb6b05f9449643a78d9873df32f
bensonchen <11488437+tomandpeter@users.noreply.github.com>
2025-05-16 19:24:41 +0800

    Update artalk-comments.js

--- assets/scripts/artalk-comments.js
+++ assets/scripts/artalk-comments.js
@@ -7,8 +7,8 @@ script.onload = function() {
     el: '#Comments',
     site: 'Kelsey 的博客',
     server: 'https://comments.kelseychen.net',
-    apiPrefix: '',  // 取消默认 /api 前缀
-    pageKey: window.location.pathname
+    apiPrefix: '',  // 不用带 /api，Worker 里都直接对路由做了兼容
+    pageKey: window.location.pathname,
+    useBackendConf: true  // 保持开启，worker 里有 /api/v2/conf 响应
   });
 }
-
